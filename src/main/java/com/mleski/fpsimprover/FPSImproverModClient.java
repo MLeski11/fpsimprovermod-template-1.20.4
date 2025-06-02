e1dd8766ec5d9621cdef603eec7fa3cb470512b2
MLeski <Mason.leski@comcast.net>
2025-06-02 12:23:16 -0400

    Initial Commit

---
+++ src/main/java/com/mleski/fpsimprover/FPSImproverModClient.java
@@ -0,0 +1,10 @@
+package com.mleski.fpsimprover;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FPSImproverModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/mleski/fpsimprover/FPSImproverModDataGenerator.java
@@ -0,0 +1,11 @@
+package com.mleski.fpsimprover;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class FPSImproverModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
